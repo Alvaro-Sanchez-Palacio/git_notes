efeac4f768167c89f152159b39cf2029355a1033
Álvaro Sánchez Palacio <alvaro.sanchez.palacio@gmail.com>
2018-12-08 20:59:40 +0100

    Added test strubtract (std, negatives, positives and mix)
    Adds tests subtract with :
    - Positive numbers
    - Negative numbers
    - Positive & Negative numbers (and vice-versa)
    
    @alvaro-sanchez-palacio

--- test_logic.py
+++ test_logic.py
@@ -8,11 +8,41 @@ class TestCalc(unittest.TestCase):
 
     def test_add(self):
         """
-        Test add fonction.
+        Test add() fonction.
         
         """
         res = add(3,2)
         self.assertEqual(res, 5)
+
+
+    def test_subtract(self):
+        """
+        Test subtract() function.
+        """
+        res = subtract(3,2)
+        self.assertEqual(res, 1)
+
+    def test_subtract_negatives(self):
+        """
+        Test subtract() function with negative numbers as inputs.
+        """
+        res = subtract(-1, -2)
+        self.assertEqual(res, 1)
+
+    def test_subtract_positive_and_negative(self):
+        """
+        Test subtract() function with a positive and a negative input.
+        """
+        res = subtract(2,-1)
+        self.assertEqual(res, 3)
+
+    def test_subtract_positive_and_negative(self):
+        """
+        Test subtract() function with a negative and a positive input.
+        """
+        res = subtract(-2, 1)
+        self.assertEqual(res, -3)
+
 
 
 if __name__ == '__main__':
